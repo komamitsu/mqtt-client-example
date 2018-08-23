110d8fad4132dc52e4c3cb14b39f477d0816f199
Mitsunori Komatsu <komamitsu@gmail.com>
2018-08-23 20:08:44 +0900

    Revert "Debugging...."
    This reverts commit 63aea17f061a0506de4cba4d2c9aeb9a2e3f6188.

--- src/main/java/org/komamitsu/mqttclient/example/MqttClientTest.java
+++ src/main/java/org/komamitsu/mqttclient/example/MqttClientTest.java
@@ -2,9 +2,6 @@ package org.komamitsu.mqttclient.example;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
-import org.eclipse.paho.client.mqttv3.IMqttActionListener;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -20,7 +17,6 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
-import java.util.concurrent.TimeUnit;
 
 public class MqttClientTest
 {
@@ -51,16 +47,16 @@ public class MqttClientTest
     private static SSLSocketFactory createSocketFactory(Path certFile, Path keyFile)
             throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException
     {
-        MqttClientUtil.KeyStorePasswordPair keyStorePasswordPair =
-                MqttClientUtil.getKeyStorePasswordPair(certFile.toString(), keyFile.toString());
+            MqttClientUtil.KeyStorePasswordPair keyStorePasswordPair =
+                    MqttClientUtil.getKeyStorePasswordPair(certFile.toString(), keyFile.toString());
 
-        // client key and certificates are sent to server so it can authenticate us
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-        kmf.init(keyStorePasswordPair.keyStore, keyStorePasswordPair.keyPassword.toCharArray());
+            // client key and certificates are sent to server so it can authenticate us
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(keyStorePasswordPair.keyStore, keyStorePasswordPair.keyPassword.toCharArray());
 
-        SSLContext context = SSLContext.getInstance("TLSv1.2");
-        context.init(kmf.getKeyManagers(), null, null);
-        return context.getSocketFactory();
+            SSLContext context = SSLContext.getInstance("TLSv1.2");
+            context.init(kmf.getKeyManagers(), null, null);
+            return context.getSocketFactory();
     }
 
     public static void main(String[] args)
@@ -73,7 +69,7 @@ public class MqttClientTest
 
         String topic        = commandArgs.topic;
         String content      = commandArgs.message;
-        int qos             = 0;
+        int qos             = 2;
         String broker       = commandArgs.endpoint;
         String clientId     = commandArgs.clientId;
 
@@ -82,62 +78,22 @@ public class MqttClientTest
         SSLSocketFactory socketFactory = createSocketFactory(commandArgs.certFile, commandArgs.keyFile);
 
         try {
-            final MqttAsyncClient client = new MqttAsyncClient(broker, MqttAsyncClient.generateClientId());
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setUserName("?SDK=Java&Version=1.2.0");
-            connOpts.setCleanSession(true);
-            connOpts.setSocketFactory(socketFactory);
-            client.connect(null,new
-                    IMqttActionListener()
-                    {
-                        @Override
-                        public void onSuccess (IMqttToken asyncActionToken){
-                            while (true) {
-                                try {
-                                    client.publish(topic, content.getBytes(), 0, false);
-                                    System.out.println("Sent hello...");
-                                    TimeUnit.SECONDS.sleep(5);
-                                }
-                                catch (MqttException e) {
-                                    e.printStackTrace();
-                                }
-                                catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onFailure (IMqttToken asyncActionToken, Throwable exception){
-                            exception.printStackTrace();
-                        }
-                    });
-
-            TimeUnit.SECONDS.sleep(30);
-
-            /*
-            MqttAsyncClient client = getClient(broker, topic, content);
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setUserName("?SDK=Java&Version=1.2.0");
             connOpts.setCleanSession(true);
             connOpts.setSocketFactory(socketFactory);
             System.out.println("Connecting to broker: "+broker);
-            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            System.out.println(connOpts.getDebug());
-            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            client.connect(connOpts);
+            sampleClient.connect(connOpts);
             System.out.println("Connected");
             System.out.println("Publishing message: "+content);
             MqttMessage message = new MqttMessage(content.getBytes());
             message.setQos(qos);
-            client.publish(topic, message);
+            sampleClient.publish(topic, message);
             System.out.println("Message published");
-            client.disconnect();
+            sampleClient.disconnect();
             System.out.println("Disconnected");
-            */
             System.exit(0);
-        }
-        catch(MqttException me) {
+        } catch(MqttException me) {
             System.out.println("reason "+me.getReasonCode());
             System.out.println("msg "+me.getMessage());
             System.out.println("loc "+me.getLocalizedMessage());
